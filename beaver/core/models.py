6495bfb830e6e0b1f2abe96a9ecc45cde92489ed
Sebastian Dahlgren <sebastian.dahlgren@avail.net>
2012-05-22 19:26:13 +0200

    Wrote new model

--- beaver/core/models.py
+++ beaver/core/models.py
@@ -12,7 +12,7 @@ class Account(models.Model):
     Definition of an Account object
     """
     def __unicode__(self):
-        return email
+        return u'%s' % self.email
 
     email               = models.EmailField(blank = False, unique = True)
     password            = models.CharField(blank = False, max_length = 50)
@@ -29,7 +29,7 @@ class Account(models.Model):
         Activate account
         """
         self.is_active = True
-        
+
         return self.save()
 
     def is_authenticated(self):
@@ -55,13 +55,89 @@ the below link in order to activate your account.
 
 Best regards
 The booking beaver team
-""" % ( self.first_name, self.email, 
+""" % ( self.first_name, self.email,
         settings.BEAVER_EXTERNAL_URL, self.activation_key, self.email)
 
             send_mail(  'Activate your Beaver account', message,
-                        settings.BEAVER_NO_REPLY_ADDRESS, [self.email], 
+                        settings.BEAVER_NO_REPLY_ADDRESS, [self.email],
                         fail_silently = False)
 
         # Save the object
         super(Account, self).save(*args, **kwargs)
+
+class BaseSchedule(models.Model):
+    """
+    Defining a base schedule
+    """
+    calendar                    = models.ForeignKey(Calendar)
     
+    monday_enabled              = models.BooleanField(blank = False, default = False)
+    monday_bookable_timespan    = models.CharField(blank = True, null = True, max_length = 40)
+    monday_not_bookable         = models.CharField(blank = True, null = True, max_length = 200)
+    tuesday_enabled             = models.BooleanField(blank = False, default = False)
+    tuesday_bookable_timespan   = models.CharField(blank = True, null = True, max_length = 40)
+    tuesday_not_bookable        = models.CharField(blank = True, null = True, max_length = 200)
+    wednesday_enabled           = models.BooleanField(blank = False, default = False)
+    wednesday_bookable_timespan = models.CharField(blank = True, null = True, max_length = 40)
+    wednesday_not_bookable      = models.CharField(blank = True, null = True, max_length = 200)
+    thursday_enabled            = models.BooleanField(blank = False, default = False)
+    thursday_bookable_timespan  = models.CharField(blank = True, null = True, max_length = 40)
+    thursday_not_bookable       = models.CharField(blank = True, null = True, max_length = 200)
+    friday_enabled              = models.BooleanField(blank = False, default = False)
+    friday_bookable_timespan    = models.CharField(blank = True, null = True, max_length = 40)
+    friday_not_bookable         = models.CharField(blank = True, null = True, max_length = 200)
+    saturday_enabled            = models.BooleanField(blank = False, default = False)
+    saturday_bookable_timespan  = models.CharField(blank = True, null = True, max_length = 40)
+    saturday_not_bookable       = models.CharField(blank = True, null = True, max_length = 200)
+    sunday_enabled              = models.BooleanField(blank = False, default = False)
+    sunday_bookable_timespan    = models.CharField(blank = True, null = True, max_length = 40)
+    sunday_not_bookable         = models.CharField(blank = True, null = True, max_length = 200)
+
+class Booking(models.Model):
+    """
+    Defines a made Booking
+    """
+    def __unicode__(self):
+        return u'%s - %s (%s - %s)' % (self.account, self.name, self.start_time, self.end_time)
+
+    account         = models.ForeignKey(Account)
+    schedule        = models.ForeignKey(Schedule)
+    
+    title           = models.CharField(blank = False, max_length = 100)
+    start           = models.DateTimeField(blank = False)
+    end             = models.DateTimeField(blank = False)
+    comment         = models.TextField(blank = True)
+    price           = models.FloatField(blank = True, null = True)
+
+class BookingType(models.Model):
+    """
+    Definition of a booking type
+    """
+    def __unicode__(self):
+        return u'%s, %i min' % (self.name, self.length)
+
+    calendar        = models.ForeignKey(Calendar)
+
+    title           = models.CharField(blank = False, max_length = 100)
+    description     = models.TextField(blank = True)
+    length          = models.IntegerField(  blank = False, max_length = 50,
+                                            help_text = 'Length in minutes')
+    price           = models.FloatField(blank = True, null = True)
+
+class Calendar(models.Model):
+    """
+    A Calendar is a collection of Schedulesapp
+    """
+    def __unicode__(self):
+        return u'%s' % self.name
+    
+    title           = models.CharField(blank = False)
+    enabled         = models.BooleanField(blank = False, default = True)
+
+class Schedule(models.Model):
+    """
+    Definition of a schedule
+    """
+    calendar        = models.ForeignKey(Calendar)
+    owner           = models.ForeignKey(Account)
+    base_schedule   = models.ForeignKey(BaseSchedule, blank = True, null = True)
